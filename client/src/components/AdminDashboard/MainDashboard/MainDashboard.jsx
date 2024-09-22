cc53df00345a3d16007ade5cbfb25f38058b8560
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-09-22 11:15:41 +0530

    create main dashboard

--- client/src/components/AdminDashboard/MainDashboard/MainDashboard.jsx
+++ client/src/components/AdminDashboard/MainDashboard/MainDashboard.jsx
@@ -1,9 +1,23 @@
-
+import React from "react";
+import SideMenu from "../MainDashboard/SideMenu";
+import AdminRoutes from "./AdminRoutes";
+import "./MainDashboard.css";
 
 const MainDashboard = () => {
   return (
-    <div>MainDashboard</div>
-  )
-}
+    <>
+      <div className="Admin_DashboardContainer">
+        <div className="Admin_SideMenuAndPageContent">
+          <div className="admin_sidebar_container">
+            <SideMenu />
+          </div>
+          <div className="Admin_PageContent">
+            <AdminRoutes />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default MainDashboard
+export default MainDashboard;
